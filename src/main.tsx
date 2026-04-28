eb4bf59109cbc9adbe6efcceedc55828d1a3cb37
H.U.C_NB <china_nb_77@163.com>
2026-04-28 10:10:00 +0800

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,22 @@
+// 强制覆盖 API 地址 - 必须在所有 import 之前
+if (typeof window !== 'undefined') {
+  window.__API_BASE_URL__ = 'http://114.215.177.52:8000';
+  window.__VISION_API_URL__ = 'http://114.215.177.52:8000/vision-api';
+  window.__PAYMENT_API_URL__ = 'http://114.215.177.52:8000/pay-api';
+  window.__AGENT_API_URL__ = 'http://114.215.177.52:8000/api';
+  (window as any).global = window;
+  localStorage.setItem('VITE_API_BASE_URL', 'http://114.215.177.52:8000');
+  localStorage.setItem('VITE_VISION_API_BASE_URL', 'http://114.215.177.52:8000/vision-api');
+  localStorage.setItem('VITE_PAYMENT_API_BASE_URL', 'http://114.215.177.52:8000/pay-api');
+}
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { RouterProvider } from 'react-router-dom';
-import { router } from './routes/router';
-import './styles.css';
+import App from './App';
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>,
+    <App />
+  </React.StrictMode>
 );
